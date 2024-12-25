aee3c042f59ff350593d5a41b32ebc67580be425
Riyy37 <domabruho123@gmail.com>
2024-12-25 08:56:21 +0200

    Program.cs

--- ConsoleApp27/Program.cs
+++ ConsoleApp27/Program.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp27
 {
+    /// <summary>
+    /// Одна штука некоторого товара стоит 20,4 руб. Напечатать таблицу стоимости 2, 3, ..., 20 штук этого товара.
+    /// </summary>
     internal class Program
     {
         static void Main(string[] args)
