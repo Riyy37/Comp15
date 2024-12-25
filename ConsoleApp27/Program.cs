e461fa46676a01d79837d7256d4af7bdb622fd5b
Riyy37 <domabruho123@gmail.com>
2024-12-25 08:54:01 +0200

    Task2

---
+++ ConsoleApp27/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Function();
+            Console.ReadKey();
+        }
+        static void Function()
+        {
+            double price = 20.4;
+            for (int i=2; i<=20; i++)
+            {
+                Console.WriteLine($"{i} шт = {price*i} руб");
+            }
+        }
+    }
+}
